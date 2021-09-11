8dea6b0949617f35b937f9024ae59c251354e9ae
soscarchillan <90418857+soscarchillan@users.noreply.github.com>
2021-09-11 12:11:57 -0300

    versión color
    Se agrega botón de cambio de color en index

--- Js/funcion.js
+++ Js/funcion.js
@@ -1,4 +1,26 @@
     alert("¡HOLA, BIENVENID@ A TU ESPACIO DE BIENESTAR...!");
       
-
+    function generarNuevoColor(){
+        var simbolos, color;
+        simbolos = "0123456789ABCDEF";
+        color = "#";
+    
+        for(var i = 0; i < 6; i++){
+            color = color + simbolos[Math.floor(Math.random() * 16)];
+        }
+    
+        document.body.style.background = color;
+        document.getElementById("hexadecimal").innerHTML = color;
+        document.getElementById("text").innerHTML = "Copiar Color";
+    }
+    
+    function copiarAlPortapapeles(hexadecimal) {
+        var aux = document.createElement("input");
+        aux.setAttribute("value", document.getElementById(hexadecimal).innerHTML);
+        document.body.appendChild(aux);
+        aux.select();
+        document.execCommand("copy");
+        document.body.removeChild(aux);
+        document.getElementById("text").innerHTML = "Copiado!";
+    }
 
