b3fa81fb62dbcc00dde19e82f8c3aede5f07ecb7
soscarchillan <90418857+soscarchillan@users.noreply.github.com>
2021-09-16 22:59:33 -0300

    version ajustes
    Nuevos ajustes jquery

---
+++ Js/jquery_funcion.js
@@ -0,0 +1,17 @@
+Fancybox.bind('[data-fancybox="gallery"]', {
+    animated: false,
+    showClass: false,
+    hideClass: false,
+  
+    click: false,
+  
+    dragToClose: false,
+  
+    Image: {
+      zoom: false,
+    },
+  
+    Toolbar: {
+      display: [{ id: "counter", position: "center" }, "close"],
+    },
+  });
